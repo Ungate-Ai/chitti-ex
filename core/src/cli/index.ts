a2688f8ccf902bbe00a247a07222b74bd85f2654
tringuyen98 <mucian.bean@gmail.com>
2024-11-11 06:28:58 +0000

    updated package.json

--- core/src/cli/index.ts
+++ core/src/cli/index.ts
@@ -76,6 +76,7 @@ export async function loadCharactersV2(agentId: string) {
     })
     .then((res) => res.json());
     if (result) {
+        console.log('result: ', result);
         const data = result.data;
         const agentName = data.name;
         const character = getCharacter(agentName);
